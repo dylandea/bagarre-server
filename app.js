12ddcd8894c314a8e93826c8f434c68631abf26e
Dylan <pinpon@ordi-pinpon.local>
2022-09-11 01:32:52 +0200

    fonctionne après session

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var session = require("express-session");
 var app = express();
+
+app.use(
+    session({
+     secret: 'a4f8071f-c873-4447-83e2',
+     resave: false,
+     saveUninitialized: false,
+    })
+    );
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ socketapi.js
@@ -0,0 +1,60 @@
+var cookie = require("cookie")
+var express = require('express');
+var session = require('express-session')
+
+
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+
+io.on("connection", function (socket) {
+
+	socket.emit("sendMessageFromBack", "New user connected"); //envoyer info comme quoi qqun s'est connecté
+
+	socket.on("sendMessage", function (message) { //recevoir/écouter
+        console.log(session.history)
+        if (session.history !== undefined) {
+            session.history = [...session.history, message]
+        } else {
+            session.history = [message]
+        }
+        
+		io.emit("sendMessageToAll", session.history); //envoyer un message à tout le monde dont soi-même
+        
+		/* socket.broadcast.emit('sendMessageToAll', "New user connected"); */ //envoi à tout le monde sauf celui qui a envoyé
+	});
+
+    socket.on("askforhistory", function (localData) { //recevoir/écouter
+        if (session.history == undefined) {
+            session.history = []
+        }
+        let locD = localData
+        if (locD == undefined) {
+            locD = []
+        }
+        
+       
+            let merge = [...session.history, ...locD]
+            let alreadyUsed = []
+            let final = []
+            merge.forEach(x=> {
+                if (!alreadyUsed.includes(x.id)) {final.push(x)}
+                alreadyUsed.push(x.id)
+            })
+            final.sort((a,b)=> new Date(a.date)-new Date(b.date))
+            session.history = final
+        
+		io.emit("history", session.history); //envoyer un message à tout le monde dont soi-même
+		/* socket.broadcast.emit('sendMessageToAll', "New user connected"); */ //envoi à tout le monde sauf celui qui a envoyé
+	});
+
+  socket.on("sendWizz", function (message) { //recevoir/écouter
+		io.emit("sendWizzToAll", message); //envoyer un message à tout le monde dont soi-même
+		/* socket.broadcast.emit('sendMessageToAll', "New user connected"); */ //envoi à tout le monde sauf celui qui a envoyé
+	});
+});
+
+
+module.exports = socketapi;
